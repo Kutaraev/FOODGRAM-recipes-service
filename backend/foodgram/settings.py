d292b17e87f2f303e03eccbbf7e6f21c2bbf9181
root <root@scholast-server.ru-central1.internal>
2021-12-06 17:19:21 +0000

    all remarks are done. v2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -69,14 +69,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'postgresql',
-        'PORT': '5432',
-    }
+    'default': env.db()
 }
 
 
