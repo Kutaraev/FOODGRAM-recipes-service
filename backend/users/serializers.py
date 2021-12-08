40d00c3a5e0a6b17e97c109df22e227c3e2ebb5a
root <root@scholast-server.ru-central1.internal>
2021-12-08 16:54:07 +0000

    subscribe 2

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,9 +1,11 @@
 from djoser.serializers import UserSerializer
+from recipes.models import Follow
 from users.models import User
 
 
 class CustomUserSerializer(UserSerializer):
     """Сериализатор для пользователей"""
+    is_subscribed = serializers.SerializerMethodField()
     class Meta:
         model = User
         fields = ('email',
@@ -20,3 +22,10 @@ class CustomUserSerializer(UserSerializer):
             user.set_password(validated_data['password'])
             user.save()
             return user
+
+        def get_is_subscribed(self, obj):
+            """Статус подписки пользователя"""
+            user = self.context.get('request').user
+            if user.is_anonymous:
+                return False
+            return Follow.objects.filter(user=user, following=obj.id).exists()
