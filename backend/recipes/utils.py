ee1fd94525413d6503dd89f1a2a8440311a42342
root <root@scholast-server.ru-central1.internal>
2021-12-08 20:36:27 +0000

    ingr

--- backend/recipes/utils.py
+++ backend/recipes/utils.py
@@ -30,9 +30,10 @@ def from_cart_to_pdf(user):
             f'{idx}. {ingr["ingredient__name"]} - {ingr["amount"]} '
             f'{ingr["ingredient__measurement_unit"]}'
         ))
-        # height -= 30
-        page.showPage()
-        page.setFont('DejaVuSerif', 24)
+        height -= 30
+        if height <=30:
+            page.showPage()
+            page.setFont('DejaVuSerif', 16)
     page.showPage()
     page.save()
     return response
